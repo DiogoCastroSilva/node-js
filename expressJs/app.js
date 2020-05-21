93f73197ccf9693f6c83ff81d774bd22f35f2875
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-05-21 20:14:38 +0100

    Add gitignore files and adding post request

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,11 +1,20 @@
 // Express
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/product', (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res) => {
+    console.log('Add Product middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
     console.log('Product middleware!');
-    res.send('<h1>Hello from Product Express</h1>');
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
