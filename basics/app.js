6e88f9a5c09c69eff37027a0aa36ad1fe1e54a61
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-05-21 10:59:33 +0100

    Adding routes in new file

--- basics/app.js
+++ basics/app.js
@@ -1,29 +1,13 @@
 const http = require('http');
+// File System
+// const fs = require('fs');
+
+// Routes
+const routes = require('./routes');
+
 
 // Creates a server
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.setHeader('Content-type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message" /> <button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-
-    }
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from node js server</h1></body>');
-    res.write('</html>');
-    res.end();
-    // Stopping the server
-    // process.exit();
-});
+const server = http.createServer(routes)
 
 // Turns on the server
 server.listen(3000);
