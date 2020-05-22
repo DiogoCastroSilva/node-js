6d5a575df782355f246257e58c3805b77155599f
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-05-22 09:31:08 +0100

    Add util to get the root file path

--- expressJs/routes/admin.js
+++ expressJs/routes/admin.js
@@ -4,10 +4,13 @@ const path = require('path');
 // Express
 const express = require('express');
 
+// Util
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
--- expressJs/routes/shop.js
+++ expressJs/routes/shop.js
@@ -4,10 +4,13 @@ const path = require('path');
 // Express
 const express = require('express');
 
+// Util
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
