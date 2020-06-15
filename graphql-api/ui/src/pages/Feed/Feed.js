8c751775ec35538244fb774d703b5012cd957e85
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-06-15 09:05:47 +0100

    Adding pagination

--- graphql-api/ui/src/pages/Feed/Feed.js
+++ graphql-api/ui/src/pages/Feed/Feed.js
@@ -58,7 +58,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
           {
-            posts {
+            posts(page: ${page}) {
               posts {
                 _id
                 title
@@ -203,7 +203,18 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.pop();
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
